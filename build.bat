0171df3430328f693a2c73ddeaa5faca7e749c5f
sleeplessghost <sleeplessghost@live.com>
2017-02-21 11:00:27 +1030

    Added build script, requires .net framework v4.*, tested in Win7

---
+++ build.bat
@@ -0,0 +1,31 @@
+@echo off
+
+if not exist dist (
+	mkdir dist
+)
+
+if exist C:\Windows\Microsoft.NET\Framework\v4.* (
+	for /d %%a in ("C:\Windows\Microsoft.NET\Framework\v4.*") do (
+		set "file=%%~fa\msbuild.exe"
+	)
+	if exist %file% (
+		echo on
+		C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ShadowverseTracker.sln
+		echo finished compiling
+		copy ShadowverseTracker\obj\debug\ShadowverseTracker.exe dist
+		copy IconChanger\obj\debug\IconChanger.exe dist
+		echo copied files to dist
+		echo done
+		echo off
+	) else (
+		echo on
+		echo could not find msbuild.exe
+		echo off
+	)
+) else (
+	echo on
+	echo could not find .net framework 4
+	echo off
+)
+
+pause>nul
